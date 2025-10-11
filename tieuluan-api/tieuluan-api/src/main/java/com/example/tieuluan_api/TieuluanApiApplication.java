78bd511f4ff296194132d83801d6c0ab919f86ff
VoPhong2503 <voquocphong6678@gmail.com>
2025-10-11 23:58:56 +0700

    back-end

---
+++ tieuluan-api/tieuluan-api/src/main/java/com/example/tieuluan_api/TieuluanApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.tieuluan_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TieuluanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TieuluanApiApplication.class, args);
+	}
+
+}
